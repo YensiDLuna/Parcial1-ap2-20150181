4ac0eb510a23c9580805645e21184bec4d1036ce
Yensi Manuel De Luna Martinez <yensi_deluna@ucne.edu.do>
2021-06-02 18:38:27 -0400

    Contexto Listo

--- Registro De Articulos/DAL/Contexto.cs
+++ Registro De Articulos/DAL/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Registro_De_Articulos.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ namespace Registro_De_Articulos.DAL
 {
     public class Contexto : DbContext
     {
+        public DbSet<Productos> Producto { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
