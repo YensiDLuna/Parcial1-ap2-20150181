bca726f7f9cac226b8d8214d06e28592f6e8d10b
Yensi Manuel De Luna Martinez <yensi_deluna@ucne.edu.do>
2021-06-02 18:19:22 -0400

    Add project files.

---
+++ Registro De Articulos/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registro_De_Articulos.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= DATA\Articulos.db");
+        }
+
+    }
+}
