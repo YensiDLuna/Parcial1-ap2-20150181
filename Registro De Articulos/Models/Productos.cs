4a203807083fed56b50363eabb285f5ffda29701
Yensi Manuel De Luna Martinez <yensi_deluna@ucne.edu.do>
2021-06-02 18:32:26 -0400

    Clase Productos

---
+++ Registro De Articulos/Models/Productos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registro_De_Articulos.Models
+{
+    public class Productos
+    {
+        public int ProductoId { get; set; }
+        public int Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public int Costo { get; set; }
+        public int ValorInventario { get; set; }
+
+    }
+}
