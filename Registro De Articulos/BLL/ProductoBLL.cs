0689d666dd5b5b1d337d9ccd0538bfc570283d10
Yensi Manuel De Luna Martinez <yensi_deluna@ucne.edu.do>
2021-06-02 19:18:30 -0400

    BLL lista

---
+++ Registro De Articulos/BLL/ProductoBLL.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_De_Articulos.DAL;
+using Registro_De_Articulos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Registro_De_Articulos.BLL
+{
+    public class ProductoBLL
+    {
+        private static object productos;
+
+
+        public static bool Guardar(Productos productos)
+        {
+            if (!Existe(productos.ProductoId))
+                return Insertar(productos);
+            else
+                return Modificar(productos);
+        }
+
+        public static bool Existe(int id)
+        {
+            Contexto context = new Contexto();
+            bool found = false;
+
+            try
+            {
+                found = context.Producto.Any(p => p.ProductoId == id);
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+            finally
+            {
+                context.Dispose();
+            }
+
+            return found;
+        }
+
+
+        private static bool Insertar(Productos productos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                contexto.Producto.Add(productos);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+
+
+        public static bool Modificar(Productos productos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                contexto.Entry(productos).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+
+                var Productos = contexto.Producto.Find(id);
+
+                if (productos != null)
+                {
+                    contexto.Producto.Remove(Productos);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static Productos Buscar(int id)
+        {
+            Contexto context = new Contexto();
+            Productos productos;
+
+            try
+            {
+                productos = context.Producto.Find(id);
+
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+            finally
+            {
+                context.Dispose();
+            }
+
+            return productos;
+
+        }
+
+
+        public static List<Productos> GetList(Expression<Func<Productos, bool>> criterio)
+        {
+            List<Productos> lista = new List<Productos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+
+                lista = contexto.Producto.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+
+
+
+
+        public static List<Productos> GetPersona()
+        {
+            List<Productos> lista = new List<Productos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Producto.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
+
+   
--- Registro De Articulos/Models/Productos.cs
+++ Registro De Articulos/Models/Productos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,16 @@ namespace Registro_De_Articulos.Models
 {
     public class Productos
     {
+        [Key]
         public int ProductoId { get; set; }
-        public int Descripcion { get; set; }
+        [Required(ErrorMessage = "Ingrese una Descripcion")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "Ingrese una exitencia ")]
         public int Existencia { get; set; }
-        public int Costo { get; set; }
-        public int ValorInventario { get; set; }
+        [Required(ErrorMessage = "Ingrese un Costo")]
+        public decimal Costo { get; set; }
+
+        public decimal ValorInventario { get; set; }
 
     }
 }
